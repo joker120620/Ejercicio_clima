d09676e9e065ec80c0182cc52757319eeec9dc66
JuanD1206 <76623625+joker120620@users.noreply.github.com>
2023-02-09 01:55:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,330 @@
+// const nombresPaises ={
+//     "BD": "Bangladesh",
+//     "BE": "Belgium",
+//     "BF": "Burkina Faso",
+//     "BG": "Bulgaria",
+//     "BA": "Bosnia and Herzegovina",
+//     "BB": "Barbados",
+//     "WF": "Wallis and Futuna",
+//     "BL": "Saint Barthelemy",
+//     "BM": "Bermuda",
+//     "BN": "Brunei",
+//     "BO": "Bolivia",
+//     "BH": "Bahrain",
+//     "BI": "Burundi",
+//     "BJ": "Benin",
+//     "BT": "Bhutan",
+//     "JM": "Jamaica",
+//     "BV": "Bouvet Island",
+//     "BW": "Botswana",
+//     "WS": "Samoa",
+//     "BQ": "Bonaire, Saint Eustatius and Saba ",
+//     "BR": "Brazil",
+//     "BS": "Bahamas",
+//     "JE": "Jersey",
+//     "BY": "Belarus",
+//     "BZ": "Belize",
+//     "RU": "Russia",
+//     "RW": "Rwanda",
+//     "RS": "Serbia",
+//     "TL": "East Timor",
+//     "RE": "Reunion",
+//     "TM": "Turkmenistan",
+//     "TJ": "Tajikistan",
+//     "RO": "Romania",
+//     "TK": "Tokelau",
+//     "GW": "Guinea-Bissau",
+//     "GU": "Guam",
+//     "GT": "Guatemala",
+//     "GS": "South Georgia and the South Sandwich Islands",
+//     "GR": "Greece",
+//     "GQ": "Equatorial Guinea",
+//     "GP": "Guadeloupe",
+//     "JP": "Japan",
+//     "GY": "Guyana",
+//     "GG": "Guernsey",
+//     "GF": "French Guiana",
+//     "GE": "Georgia",
+//     "GD": "Grenada",
+//     "GB": "United Kingdom",
+//     "GA": "Gabon",
+//     "SV": "El Salvador",
+//     "GN": "Guinea",
+//     "GM": "Gambia",
+//     "GL": "Greenland",
+//     "GI": "Gibraltar",
+//     "GH": "Ghana",
+//     "OM": "Oman",
+//     "TN": "Tunisia",
+//     "JO": "Jordan",
+//     "HR": "Croatia",
+//     "HT": "Haiti",
+//     "HU": "Hungary",
+//     "HK": "Hong Kong",
+//     "HN": "Honduras",
+//     "HM": "Heard Island and McDonald Islands",
+//     "VE": "Venezuela",
+//     "PR": "Puerto Rico",
+//     "PS": "Palestinian Territory",
+//     "PW": "Palau",
+//     "PT": "Portugal",
+//     "SJ": "Svalbard and Jan Mayen",
+//     "PY": "Paraguay",
+//     "IQ": "Iraq",
+//     "PA": "Panama",
+//     "PF": "French Polynesia",
+//     "PG": "Papua New Guinea",
+//     "PE": "Peru",
+//     "PK": "Pakistan",
+//     "PH": "Philippines",
+//     "PN": "Pitcairn",
+//     "PL": "Poland",
+//     "PM": "Saint Pierre and Miquelon",
+//     "ZM": "Zambia",
+//     "EH": "Western Sahara",
+//     "EE": "Estonia",
+//     "EG": "Egypt",
+//     "ZA": "South Africa",
+//     "EC": "Ecuador",
+//     "IT": "Italy",
+//     "VN": "Vietnam",
+//     "SB": "Solomon Islands",
+//     "ET": "Ethiopia",
+//     "SO": "Somalia",
+//     "ZW": "Zimbabwe",
+//     "SA": "Saudi Arabia",
+//     "ES": "Spain",
+//     "ER": "Eritrea",
+//     "ME": "Montenegro",
+//     "MD": "Moldova",
+//     "MG": "Madagascar",
+//     "MF": "Saint Martin",
+//     "MA": "Morocco",
+//     "MC": "Monaco",
+//     "UZ": "Uzbekistan",
+//     "MM": "Myanmar",
+//     "ML": "Mali",
+//     "MO": "Macao",
+//     "MN": "Mongolia",
+//     "MH": "Marshall Islands",
+//     "MK": "Macedonia",
+//     "MU": "Mauritius",
+//     "MT": "Malta",
+//     "MW": "Malawi",
+//     "MV": "Maldives",
+//     "MQ": "Martinique",
+//     "MP": "Northern Mariana Islands",
+//     "MS": "Montserrat",
+//     "MR": "Mauritania",
+//     "IM": "Isle of Man",
+//     "UG": "Uganda",
+//     "TZ": "Tanzania",
+//     "MY": "Malaysia",
+//     "MX": "Mexico",
+//     "IL": "Israel",
+//     "FR": "France",
+//     "IO": "British Indian Ocean Territory",
+//     "SH": "Saint Helena",
+//     "FI": "Finland",
+//     "FJ": "Fiji",
+//     "FK": "Falkland Islands",
+//     "FM": "Micronesia",
+//     "FO": "Faroe Islands",
+//     "NI": "Nicaragua",
+//     "NL": "Netherlands",
+//     "NO": "Norway",
+//     "NA": "Namibia",
+//     "VU": "Vanuatu",
+//     "NC": "New Caledonia",
+//     "NE": "Niger",
+//     "NF": "Norfolk Island",
+//     "NG": "Nigeria",
+//     "NZ": "New Zealand",
+//     "NP": "Nepal",
+//     "NR": "Nauru",
+//     "NU": "Niue",
+//     "CK": "Cook Islands",
+//     "XK": "Kosovo",
+//     "CI": "Ivory Coast",
+//     "CH": "Switzerland",
+//     "CO": "Colombia",
+//     "CN": "China",
+//     "CM": "Cameroon",
+//     "CL": "Chile",
+//     "CC": "Cocos Islands",
+//     "CA": "Canada",
+//     "CG": "Republic of the Congo",
+//     "CF": "Central African Republic",
+//     "CD": "Democratic Republic of the Congo",
+//     "CZ": "Czech Republic",
+//     "CY": "Cyprus",
+//     "CX": "Christmas Island",
+//     "CR": "Costa Rica",
+//     "CW": "Curacao",
+//     "CV": "Cape Verde",
+//     "CU": "Cuba",
+//     "SZ": "Swaziland",
+//     "SY": "Syria",
+//     "SX": "Sint Maarten",
+//     "KG": "Kyrgyzstan",
+//     "KE": "Kenya",
+//     "SS": "South Sudan",
+//     "SR": "Suriname",
+//     "KI": "Kiribati",
+//     "KH": "Cambodia",
+//     "KN": "Saint Kitts and Nevis",
+//     "KM": "Comoros",
+//     "ST": "Sao Tome and Principe",
+//     "SK": "Slovakia",
+//     "KR": "South Korea",
+//     "SI": "Slovenia",
+//     "KP": "North Korea",
+//     "KW": "Kuwait",
+//     "SN": "Senegal",
+//     "SM": "San Marino",
+//     "SL": "Sierra Leone",
+//     "SC": "Seychelles",
+//     "KZ": "Kazakhstan",
+//     "KY": "Cayman Islands",
+//     "SG": "Singapore",
+//     "SE": "Sweden",
+//     "SD": "Sudan",
+//     "DO": "Dominican Republic",
+//     "DM": "Dominica",
+//     "DJ": "Djibouti",
+//     "DK": "Denmark",
+//     "VG": "British Virgin Islands",
+//     "DE": "Germany",
+//     "YE": "Yemen",
+//     "DZ": "Algeria",
+//     "US": "United States",
+//     "UY": "Uruguay",
+//     "YT": "Mayotte",
+//     "UM": "United States Minor Outlying Islands",
+//     "LB": "Lebanon",
+//     "LC": "Saint Lucia",
+//     "LA": "Laos",
+//     "TV": "Tuvalu",
+//     "TW": "Taiwan",
+//     "TT": "Trinidad and Tobago",
+//     "TR": "Turkey",
+//     "LK": "Sri Lanka",
+//     "LI": "Liechtenstein",
+//     "LV": "Latvia",
+//     "TO": "Tonga",
+//     "LT": "Lithuania",
+//     "LU": "Luxembourg",
+//     "LR": "Liberia",
+//     "LS": "Lesotho",
+//     "TH": "Thailand",
+//     "TF": "French Southern Territories",
+//     "TG": "Togo",
+//     "TD": "Chad",
+//     "TC": "Turks and Caicos Islands",
+//     "LY": "Libya",
+//     "VA": "Vatican",
+//     "VC": "Saint Vincent and the Grenadines",
+//     "AE": "United Arab Emirates",
+//     "AD": "Andorra",
+//     "AG": "Antigua and Barbuda",
+//     "AF": "Afghanistan",
+//     "AI": "Anguilla",
+//     "VI": "U.S. Virgin Islands",
+//     "IS": "Iceland",
+//     "IR": "Iran",
+//     "AM": "Armenia",
+//     "AL": "Albania",
+//     "AO": "Angola",
+//     "AQ": "Antarctica",
+//     "AS": "American Samoa",
+//     "AR": "Argentina",
+//     "AU": "Australia",
+//     "AT": "Austria",
+//     "AW": "Aruba",
+//     "IN": "India",
+//     "AX": "Aland Islands",
+//     "AZ": "Azerbaijan",
+//     "IE": "Ireland",
+//     "ID": "Indonesia",
+//     "UA": "Ukraine",
+//     "QA": "Qatar",
+//     "MZ": "Mozambique"
+//  }
+function mostrarRes(temp,min,max,senTerm,humedad, ciudad, est_temp, des_temp, icon) {
+    let conta_res=document.querySelector("#respuesta")
+    document.querySelector("#city").innerHTML=ciudad
+    document.querySelector("#est_temp").innerHTML =est_temp
+    document.querySelector("#des_temp").innerHTML =des_temp
+    conta_res.setAttribute("class", "respuesta")
+    document.getElementById("img_res").setAttribute('src', icon)
+    let p1=document.querySelector("#temp");
+    let p2=document.querySelector("#temp_min");
+    let p3=document.querySelector("#temp_max");
+    let p4=document.querySelector("#senTerm");
+    let p5=document.querySelector("#humedad");
+    p1.innerHTML=temp;
+    p2.innerHTML=min;
+    p3.innerHTML=max;
+    p4.innerHTML=senTerm;
+    p5.innerHTML=humedad;
+}
+async function traer(city){
+    const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
+    let res=  await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`)
+    res = await res.json();
+    if (city!=="") {
+      if(res.cod==404){
+        document.querySelector("#msj").setAttribute("class","msj")
+        setTimeout(()=>{
+            document.querySelector("#msj").setAttribute("class","ocultar")
+        }, 3000)
+      
+      }
+      let temp = res.main.temp;
+      temp = convGrados(temp);
+      let est_temp=res.weather[0].main
+      let des_temp=res.weather[0].description;
+      const icon = `https://openweathermap.org/img/wn/${res.weather[0].icon}.png`;
+      let temp_min = convGrados(res.main.temp_min);
+      let temp_max = convGrados(res.main.temp_max);
+      let senTerm = convGrados(res.main.feels_like);
+      let humedad = res.main.humidity;
+      mostrarRes(temp, temp_min, temp_max, senTerm, humedad, res.name, est_temp, des_temp, icon);
+    }else{
+        document.querySelector("#alert").setAttribute("class","msj")
+        setTimeout(()=>{
+            document.querySelector("#alert").setAttribute("class","ocultar")
+        }, 3000)
+    }
+     
+}
+// function crearElemento(padre,nombre) {
+//     padre=document.getElementById(`${padre}`)
+//     let option = document.createElement("option");
+//     let nombreOp = document.createTextNode(nombre);
+//     option.appendChild(nombreOp);
+//     option.setAttribute("value",nombre);
+//     padre.appendChild(option);  
+// }
+// //function crearLista(nombresPaises) {
+//     let nomPaises = Object.values(nombresPaises);
+//     for(let i=0; i< nomPaises.length; i++){
+//         nomPaises.sort()
+//         crearElemento("padre",nomPaises[i])
+         
+// }
+// //}
+function convGrados(kelvin) {
+    let kelvinc=(kelvin-273.15).toFixed();
+    kelvinc=kelvinc+ "°C"
+    return  kelvinc
+
+}
+//crearLista(nombresPaises);
+
+//let pais=document.querySelector("#padre")
+let ciudad=document.querySelector("#ciudad")
+let btnEnviar=document.querySelector("#btnEnviar")
+btnEnviar.addEventListener("click",()=>{
+    traer(ciudad.value)
+})
